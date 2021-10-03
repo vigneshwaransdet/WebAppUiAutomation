6f830698d0bce23b277de2c3a3118d5dec0c4d88
Vicky Sony <vigneshwaransdet@gmail.com>
2021-10-03 17:07:22 +0530

    WebApi Push

---
+++ src/main/java/tests/Login.java
@@ -0,0 +1,33 @@
+package tests;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login{
+	
+	
+
+	@Test()
+	public void login() {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://localhost:9002/");
+		driver.findElement(By.xpath("//span[contains(text(),'use your account')]/following-sibling::input[@type='username1']")).sendKeys("babu@testleaf.com");
+		driver.findElement(By.xpath("//span[contains(text(),'use your account')]/following-sibling::input[@type='password']")).sendKeys("tester@123");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+		driver.close();
+		
+	}
+	
+	
+}
